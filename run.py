a1083154f9217b8384cf526e0646da8708863ddf
Bruno Nicholas <sbnibro256@gmail.com>
2018-11-27 03:04:12 +0300

    stating the design of the endpoints

---
+++ run.py
@@ -0,0 +1,14 @@
+from flask import Flask, Response, request
+import os
+
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24) 
+
+@app.route('/')
+def index():
+    return ('Welcome to my API!')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5060,debug=True)
