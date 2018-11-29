d5bf5f93eda99450548c75fb3e7a6cc57a967646
Bruno Nicholas <sbnibro256@gmail.com>
2018-11-29 11:36:21 +0300

    finalizing with delete feature and adding of tests repository[#162253696]

---
+++ tests/test_interventions.py
@@ -0,0 +1,18 @@
+# test_interventions.py
+
+from app import app
+import unittest
+import json
+
+class InterventionTestCase(unittest.TestCase):
+	def setup(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def test_route_indices(self):
+		pass
+
+if __name__ == '__main__':
+    unittest.main()		
---
+++ tests/test_users.py
@@ -0,0 +1,18 @@
+# test_users.py
+
+from app import app
+import unittest
+import json
+
+class UserTestCase(unittest.TestCase):
+	def setup(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def test_route_indices(self):
+		pass
+
+if __name__ == '__main__':
+    unittest.main()
